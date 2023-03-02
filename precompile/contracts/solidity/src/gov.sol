d6e923be58eb5681b92695625d8558f6a349495f
Never_Defined <hk-nft@protonmail.com>
2023-03-02 14:54:50 -0500

    wip

---
+++ precompile/contracts/solidity/src/gov.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2023, Berachain Foundation. All rights reserved.
+// Use of this software is govered by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
+pragma solidity ^0.8.4;
+
+interface IGovernanceModule {
+    ////////////////////////////////////////// Write Methods /////////////////////////////////////////////
+    function submitProposal(
+        bytes calldata message,
+        Coin[] memory initialDeposit,
+        string memory proposer,
+        string memory metadata,
+        string memory title,
+        string memory summary,
+        bool expedited
+    ) external returns (uint256);
+
+    function cancelProposal(uint256 proposalId, string memory proposer)
+        external
+        returns (
+            uint256,
+            uint256,
+            uint256
+        );
+
+    function execLegacyContent(bytes calldata content, string memory authority)
+        external;
+
+    //////////////////////////////////////////// UTILS ////////////////////////////////////////////
+    /**
+     * @dev Represents a cosmos coin.
+     */
+    struct Coin {
+        uint256 amount;
+        uint256 denom;
+    }
+}
